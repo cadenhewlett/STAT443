307a59a9403ee7e3399c11968ad1b867a3302b1f
Caden Hewlett <thetroutmanishere@gmail.com>
2024-01-12 11:38:25 -0800

    up to q3 done

--- labs/Rough_Lab1.R
+++ labs/Rough_Lab1.R
@@ -9,3 +9,11 @@ ggplot(dat, aes(x = 1:nrow(dat), y = LakeLevel)) +
   ylab("Lake Level (Depth, metres)") +
   ggtitle("Plot of \"LakeLevel\" Variable") + 
   theme_bw()
+
+tail(dat)
+x <- ts(dat$LakeLevel, 
+        start = c(2007, 1), 
+        end = c(2012, 1), # note: (2011, 12) omitted december
+        frequency = 365)
+plot(x)
+length(x) == nrow(dat)
