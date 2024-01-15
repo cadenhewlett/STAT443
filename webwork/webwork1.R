7ea0b66d6b77aa3bc4bed802b6e6f9128b527cd7
Caden Hewlett <cadenh139@edu.sd45.bc.ca>
2024-01-14 18:12:31 -0800

    moving on to q1 (idk how to do q2)

--- webwork/webwork1.R
+++ webwork/webwork1.R
@@ -2,12 +2,20 @@ library(zoo)
 library(tseries)
 df <- read.csv("webwork/file1d174cf8d84.csv")
 
-?acf
+# get df info so that we can make the tseries
 tail(df)
 series <- ts(df$Value, 
         start = c(1972, 1), 
-        end = c(2014, 9),
+        #  end = c(2014, 9),
         frequency = 12)
 length(series)
 nrow(df)
 plot(series)
+
+###### QUESTION 2.1 #########
+
+acf_result = acf(series)
+# extract autocorrelation coefficient 1
+r1 = acf_result$acf[2]
+r1
+
